b4be1d1c13404f53e2acf0d8a961525de7761f1c
simonachen11 <46848408+simonachen11@users.noreply.github.com>
2021-12-08 09:26:13 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+// #include "Sim.h"
+
+// The following is an example of how you might set up your main function (Sim.h is an object that inherits from World)
+
+int main (int argc, char **argv) { 
+	// Sim w;
+	// w.PrintGrid();
+
+	// while(! w.Extinction())
+	// {
+	//	w.UpdateGrid();
+	//	w.PrintGrid();
+		
+	//	char in; 
+	
+	//	cout << "See next day? (y,n) "<< endl;
+	//	cin >> in; 
+	//	if(in != 'y') break;
+	// }
+	// cout << "Brainsssssssss!" << endl;
+	return 0;
+}
